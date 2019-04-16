87806b609d0b3b32393edd7585ab9d0ea47e5b71
Roberto Puga <rg.pugafranco@gmail.com>
2019-04-16 21:20:38 +0000

    implement hello world

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+#create your tests here
 
-# Create your tests here.
+class HelloWorldTest(TestCase):
+	
+  def test_reachable(self):
+	#given
+	client = Client()
+
+	#when 
+	response = client.get("/hello_world")
+
+	#then 
+	self.assertEqual(301, response.status_code)
+
+  def test_hello_world(self):
+
+	client = Client()
+	response = client.get("/hello_world/")
+	self.assertIn("Hello world !", response.content.decode('utf-8'))
